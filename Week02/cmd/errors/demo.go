8024ef24febb267408c8f5d51bd2bef495160fa3
dowenliu-xyz <hawkdowen@hotmail.com>
2020-12-01 08:22:04 +0800

    github.com/pkg/errors demo

---
+++ Week02/cmd/errors/demo.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"github.com/pkg/errors"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+)
+
+func ReadFile(path string) ([]byte, error) {
+	f, err := os.Open(path)
+	if err != nil {
+		return nil, errors.Wrap(err, "open failed")
+	}
+	defer f.Close()
+	buf, err := ioutil.ReadAll(f)
+	if err != nil {
+		return nil, errors.Wrap(err, "read failed")
+	}
+	return buf, nil
+}
+
+func ReadConfig() ([]byte, error) {
+	home := os.Getenv("HOME")
+	config, err := ReadFile(filepath.Join(home, ".setting.xml"))
+	return config, errors.WithMessage(err, "could not read config")
+}
+
+func main() {
+	_, err := ReadConfig()
+	if err != nil {
+		fmt.Printf("The err: %v\n", err)
+		fmt.Printf("original error: %T %v\n", errors.Cause(err), errors.Cause(err))
+		fmt.Printf("stack trace:\n%+v\n", err)
+		os.Exit(1)
+	}
+}
--- Week02/go.mod
+++ Week02/go.mod
@@ -1,3 +1,5 @@
 module github.com/dowenliu-xyz/Go-000/Week02
 
 go 1.15
+
+require github.com/pkg/errors v0.9.1
